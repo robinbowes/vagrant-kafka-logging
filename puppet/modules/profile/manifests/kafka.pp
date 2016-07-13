9c834eee431f7d5c81715a8cafa57082fe0d35e2
Robin Bowes <rbowes@williamhill.co.uk>
2016-07-13 14:41:56 +0100

    Install kakfa

--- puppet/modules/profile/manifests/kafka.pp
+++ puppet/modules/profile/manifests/kafka.pp
@@ -1,3 +1,13 @@
+# install kafka
 class profile::kafka {
+
+  class { 'kafka':
+    version       => '0.10.0.0',
+    scala_version => '2.11',
+    install_java  => true,
+  }
+  class { 'kafka::broker':
+    config => { 'broker.id' => '0', 'zookeeper.connect' => 'zookeeper.int.yo61.net:2181' }
+  }
 
 }
